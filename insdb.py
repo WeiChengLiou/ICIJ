3db0f8661f2563c3182a291368dea2c87c5a8c96
Gilbert Liou <gilbert.liou.tw@gmail.com>
2014-09-04 11:25:39 +0800

    add path to insdb

--- insdb.py
+++ insdb.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import csv
+import os
 from pdb import set_trace
 from utils import *
 from work import *
@@ -9,18 +10,18 @@ from work import *
 
 def chkfile(fi):
     with open(fi, 'rb') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=';', quotechar='"')
-        for i, row in enumerate(spamreader):
+        csvreader = csv.reader(csvfile, delimiter=';', quotechar='"')
+        for i, row in enumerate(csvreader):
             print u', '.join([x.decode('utf8') for x in row])
             if i >= 10:
                 break
 
 
-def filespec(fis):
+def filespec(path, fis):
     fspec = open('filespec.txt', 'wb')
     for fi in fis:
         fspec.write('#%s\n' % fi)
-        with open(fi, 'rb') as csvfi:
+        with open(os.path.join(path, fi), 'rb') as csvfi:
             rows = csv.reader(csvfi, delimiter=';', quotechar='"')
             for row in rows:
                 fspec.write(u',\n'.join([unicode(x).decode('utf8') for x in row]))
@@ -37,26 +38,31 @@ def insdb(coll, fi):
             if cols is None:
                 cols = [x for x in row]
             else:
-                dic = {k:v.decode('utf8') for k,v in zip(cols, row)}
+                dic = {k: v.decode('utf8') for k, v in zip(cols, row)}
                 coll.insert(dic)
                 #print dic
                 #break
 
 
-if __name__ == '__main__':
-
+def refreshDB():
+    path = '../csv 2014 01 23'
     fis = ['countriesNW.csv',
            'edges_1DNW.csv',
            'node_countriesNW.csv',
            'nodesNW.csv']
     tbls = [cn.nations, cn.edges, cn.nodenations, cn.nodes]
 
-    #filespec(fis)
+    filespec(path, fis)
 
-    #for tbl, fi in zip(tbls, fis):
-        #print fi
-        #chkfile(fi)
-        #insdb(tbl, fi)
+    for tbl, fi in zip(tbls, fis):
+        fi1 = os.path.join(path, fi)
+        print fi1
+        chkfile(fi1)
+        insdb(tbl, fi1)
         #print 'press enter...'
         #raw_input()
+
+
+if __name__ == '__main__':
+    refreshDB()
 
